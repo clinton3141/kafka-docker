fef17aebed9b16e55a4c92ff19e81e13fbf02cab
Clinton Montague <clinton.montague@kaluza.com>
2021-11-10 17:21:03 +0000

    internal/external comms work

--- kafka.js
+++ kafka.js
@@ -1,45 +1,32 @@
-// import the `Kafka` instance from the kafkajs library
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Kafka } = require('kafkajs');
 
-// the client ID lets kafka know who's producing the messages
-const clientId = 'my-app';
-// we can define the list of brokers in the cluster
+const clientId = 'test-app';
 const brokers = ['127.0.0.1:9092'];
-// this is the topic to which we want to write messages
-const topic = 'message-log';
+const topic = 'test-log';
 
-// initialize a new kafka client and initialize a producer from it
 const kafka = new Kafka({ clientId, brokers });
 const producer = kafka.producer();
 
-// we define an async function that writes a new message each second
 const produce = async () => {
   await producer.connect();
-  let i = 0;
-
-  // after the produce has connected, we start an interval timer
-  setInterval(async () => {
+  const randomValues = Array(1000).fill(null).map(x => Math.random());
+  await Promise.all(randomValues.map(async (v, i) => {
     try {
-      // send a message to the configured topic with
-      // the key and value formed from the current value of `i`
       await producer.send({
         topic,
         messages: [
           {
-            key: String(i),
-            value: 'this is message ' + i,
-          },
-        ],
+            key: i.toString(),
+            value: v.toString(),
+          }
+        ]
       });
-
-      // if the message is written successfully, log it and increment `i`
-      console.log('writes: ', i);
-      i++;
-    } catch (err) {
-      console.error('could not write message ' + err);
+      console.log(`sent message ${i}`);
+    } catch (e) {
+      console.error(`could not send message. ${err}`);
     }
-  }, 1000);
+  }));
+  await producer.disconnect();
 };
 
-produce();
+produce().then(() => console.log('done')).catch(e => console.log(e));
